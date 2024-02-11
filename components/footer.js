9903ac1f0673145a65134aed58f2aa731560d0db
alexandre-bismuth <alexandre.bismuth@polytechnique.edu>
2024-02-11 12:14:08 +0100

    Visual fixes

--- components/footer.js
+++ components/footer.js
@@ -5,11 +5,11 @@ import Link from 'next/link';
 export default function Footer() {
     return (
         <footer className="bg-white p-4">
-            <div className="flex items-start justify-between space-x-2 mx-40 mt-4" style={{ height: "170px" }}>
+            <div className="flex items-start justify-between space-x-2 mx-4 md:mx-40 mt-4" style={{ height: "170px" }}>
                 <div className="flex flex-col">
                     <div className="flex items-center">
                         <Image src="/logos/alt-logo.svg" alt="Alt Workspaces Logo" width={25} height={25} style={{ cursor: 'pointer' }} />
-                        <div className='font-ClashSemiBold text-xl ml-3' style={{ letterSpacing: '0.5px' }}> Alt Workspaces </div>
+                        <div className='font-ClashSemiBold text-xl ml-3 mr-10' style={{ letterSpacing: '0.5px' }}> Alt Workspaces </div>
                     </div>
                     <div className='font-InterRegular text-sm text-gray-500 mt-2'>Automate your business with us.</div>
                 </div>
--- components/header.js
+++ components/header.js
@@ -10,7 +10,7 @@ export default function Header() {
         <header className="flex items-center justify-between bg-white py-5 px-8 fixed w-full z-50">
             <Image src="/logos/alt-logo.svg" alt="Alt Workspaces Logo" width={30} height={30} style={{ cursor: 'pointer' }} />
             <div className='font-ClashSemiBold text-2xl ml-3'> Alt Workspaces </div>
-            <nav className="flex-grow flex justify-center items-center font-InterRegular">
+            <nav className="flex-grow hidden lg:flex justify-center items-center font-InterRegular">
                 <Link href="/" passHref><span className={`mx-10 px-2 py-1 ${pathname === '/' ? 'text-main-purple' : ''} hover:text-main-purple cursor-pointer`}>Home</span></Link>
                 <Link href="/services" passHref><span className={`mx-10 px-2 py-1 ${pathname === '/services' ? 'text-main-purple' : ''} hover:text-main-purple cursor-pointer`}>Services</span></Link>
                 <Link href="/products" passHref><span className={`mx-10 px-2 py-1 ${pathname === '/products' ? 'text-main-purple' : ''} hover:text-main-purple cursor-pointer`}>Products</span></Link>
